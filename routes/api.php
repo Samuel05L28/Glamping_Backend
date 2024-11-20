8d5eaaf67f71dc454f8e9569b11d61be07a929e1
ArenasAg <arenasanderson63@gmail.com>
2024-11-20 17:02:28 -0500

    Auth #8

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,33 @@
 use App\Http\Controllers\CabinController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\v1\AuthController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
 //Poner nombres a todas las rutas 
-Route::get('/hola/locos' , [CabinController::class ,'index'])->name("hola.locos");
+Route::get('/hola/locos', [CabinController::class, 'index'])->name("hola.locos");
 
-Route::apiResource('cabins',CabinController::class);
+Route::post(
+    '/v1/login',
+    [
+        App\Http\Controllers\api\v1\AuthController::class,
+        'login'
+    ]
+)->name('api.login');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post(
+        '/v1/logout',
+        [
+            App\Http\Controllers\api\v1\AuthController::class,
+            'logout'
+        ]
+    )->name('api.logout');
+});
+
+Route::middleware(['auth:sanctum'])->group(function() {
+    Route::apiResource('cabins', CabinController::class);
+});
